a5dea9e4823d5eda98ddbd20e8c6d32325cefab7
Aina-hub <hafizaaina39@gmail.com>
2021-06-18 06:13:42 +0000

    commit for 6.3 server

---
+++ 6.3.py
@@ -0,0 +1,48 @@
+import socket
+import sys
+import time
+import errno
+import math         #calculation
+from multiprocessing import Process
+
+ok_message = '\nHTTP/1.0 200 OK\n\n'
+nok_message = '\nHTTP/1.0 404 NotFound\n\n'
+
+def process_start(s_sock):
+    s_sock.send(str.encode("Online Python Calculator (LOG, SQUARE ROOT, EXPONENTIAL)\n--------------- How to use: log/sqrt/exp <number>----------------\n**************** Example: log 123*************\n\t\tType 'exit' to close"))
+    while True:
+        data = s_sock.recv(2048)                        #input received from client
+        data = data.decode("utf-8")
+        
+        #calculation
+        try:
+            operation, value = data.split()
+            op = str(operation)
+            num = int(value)
+        
+            if op[0] == 'l':
+                op = 'Log'
+                answer = math.log10(num)
+            elif op[0] == 's':
+                op = 'Square root'
+                answer = math.sqrt(num)
+            elif op[0] == 'e':
+                op = 'Exponential'
+                answer = math.exp(num)
+            else:
+                answer = ('ERROR')
+        
+            sendAnswer = (str(op) + '(' + str(num) + ') = ' + str(answer))
+            print ('Calculation successful!')
+        except:
+            print ('Invalid input')
+            sendAnswer = ('Invalid input')
+    
+        #s_sock.send(str.encode(sendAnswer))
+        
+        if not data:
+            break
+            
+        s_sock.send(str.encode(sendAnswer))
+        #s_sock.sendall(str.encode(ok_message))
+    s_sock.close()
